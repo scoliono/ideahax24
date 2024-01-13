f5182c1965452b8e00d30fe2131139783164fad6
James Shiffer <2191476+scoliono@users.noreply.github.com>
2024-01-13 13:32:04 -0800

    JS form serializations

--- src/MCQuestions.js
+++ src/MCQuestions.js
@@ -1,20 +1,30 @@
 import json from './questions.json';
 import Form from 'react-bootstrap/Form';
 
+function oneHot(i, choices) {
+    let str = [];
+    for (let j = 0; j < choices.length; ++j) {
+        str.push("0");
+    }
+    str[i] = "1";
+    return str.join("");
+}
+
 function MCQuestions() {
     const questions = [];
     for (const question of json) {
         questions.push(
-            <li>
-                <Form.Group className="mb-3">
+            <li key={question.name} className="m-auto">
+                <Form.Group className="m-auto mb-5 w-50 text-left">
                     <Form.Label>{question.label}</Form.Label>
                     {question.options.map((opt, i) => {
                         return (
                             <Form.Check
+                                key={`${question.name}_choice_${i}`}
                                 required
                                 type="radio"
                                 name={question.name}
-                                value={i}
+                                value={oneHot(i, question.options)}
                                 label={opt}
                             />
                         );
